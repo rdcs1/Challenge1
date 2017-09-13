c33c67f5799b22d26bb8c500ef3550889ae8c8c1
rdcs1 <rdcs1@hotmail.com>
2017-09-13 17:29:11 -0500

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,11 @@ export class AppComponent implements OnInit{
   author='David Caro'
 
   ngOnInit() {
+    this.eyes = 'eyes'+Math.floor(Math.random()*10);
+    this.nose = 'nose'+Math.floor(Math.random()*9);
+    this.mouth = 'mouth'+Math.floor(Math.random()*11);
+    this.color = '0123456789abcdef'.split('').map(function(v,i,a){
+      return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
     setInterval(()=>{        
       this.eyes = 'eyes'+Math.floor(Math.random()*10);
       this.nose = 'nose'+Math.floor(Math.random()*9);
