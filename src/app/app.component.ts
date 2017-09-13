b44bc1d6a0e962b07e794dd929fdd6c12663184c
rdcs1 <rdcs1@hotmail.com>
2017-09-13 16:56:17 -0500

    Changes to be committed: 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  eyes;
+  nose;
+  mouth;
+  color;
+  author='David Caro'
+
+  ngOnInit() {
+    setInterval(()=>{        
+      this.eyes = 'eyes'+Math.floor(Math.random()*10);
+      this.nose = 'nose'+Math.floor(Math.random()*9);
+      this.mouth = 'mouth'+Math.floor(Math.random()*11);
+      this.color = '0123456789abcdef'.split('').map(function(v,i,a){
+        return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
+    }, 2000)
+  }
   title = 'app';
+
 }
+
